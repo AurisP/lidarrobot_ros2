86ef00034a8b7d6ee193579bc15333e4409702ff
Auris <aurispraam@gmail.com>
2023-02-07 09:37:14 +0100

    Switched to ros2_control

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -2,7 +2,6 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
-
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
@@ -41,11 +40,24 @@ def generate_launch_description():
              executable='rviz2',
              arguments=['-d', os.path.join(get_package_share_directory(package_name), 'rviz', 'urdf.rviz')])
     
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"],
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
+    )
 
     # Launch them all!
     return LaunchDescription([
         rsp,
         gazebo,
-        spawn_entity,
-        rviz,
+        spawn_entity,  
+        diff_drive_spawner,
+        joint_broad_spawner,
+        # rviz,
     ])
